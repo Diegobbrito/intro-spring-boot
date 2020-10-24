a27a8059682e8c3bbd1ac04dd142afedce8a4df0
Diego Brito <dibbrito@gmail.com>
2020-10-23 23:54:39 -0300

    Created First Routes

--- src/main/java/com/dev/introspringmvc/SpringMvcConfig.java
+++ src/main/java/com/dev/introspringmvc/SpringMvcConfig.java
@@ -1,17 +1,18 @@
-package com.dev.introspringmvc.config;
+package com.dev.introspringmvc;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan("com.dev.introspringmvc")
-public class SpringMvcConfig {
+public class SpringMvcConfig implements WebMvcConfigurer {
     @Bean
     public ViewResolver internalResourceViewResolver() {
         InternalResourceViewResolver bean = new InternalResourceViewResolver();
