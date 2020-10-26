7574d7f8e68b89b0562f0864d734ff3f54fe7c0f
Diego Brito <dibbrito@gmail.com>
2020-10-25 21:41:55 -0300

    Add update method

--- src/main/java/com/dev/introspringmvc/web/controller/UsuarioController.java
+++ src/main/java/com/dev/introspringmvc/web/controller/UsuarioController.java
@@ -5,10 +5,7 @@ import com.dev.introspringmvc.domain.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -35,5 +32,20 @@ public class UsuarioController {
         dao.salvar(usuario);
         attr.addFlashAttribute("message", "Usuário salvo com sucesso");
         return "redirect:/usuario/todos";
+    }
+
+    @GetMapping("/update/{id}")
+    public ModelAndView preUpdate(@PathVariable("id") Long id, ModelMap model){
+        Usuario usuario = dao.getId(id);
+
+        model.addAttribute("usuario", usuario);
+        return new ModelAndView("/user/add", model);
+    }
+
+    @PostMapping("/update")
+    public ModelAndView update(@ModelAttribute("usuario") Usuario usuario, RedirectAttributes attr){
+        dao.editar(usuario);
+        attr.addFlashAttribute("message", "Usuário atualizado com sucesso");
+        return new ModelAndView("redirect:/usuario/todos");
     }
 }
